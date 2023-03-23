ef63061153327e6455511049fba1d6658c175d0c
caescobar06 <128372244+caescobar06@users.noreply.github.com>
2023-03-23 15:36:05 -0600

    Create EJERCICIO 4 ADD.C

---
+++ EJERCICIO 4 ADD.C
@@ -0,0 +1,22 @@
+#include "add.h" //importa la librería de las funciones predefinidas
+
+
+int suma (int a, int b)
+{
+    return a+b;
+}
+
+int resta (int a, int b)
+{
+    return a-b;
+}
+
+float multi (int a, int b)
+{
+    return a*b;
+}
+
+float divi (int a, int b)
+{
+    return a/b;
+}
